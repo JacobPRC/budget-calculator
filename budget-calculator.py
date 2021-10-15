9da4645e7dd46e6ffb6b84cd1d1ef507b1f7cdfd
jacob <jacobcunningham96@yahoo.com>
2021-10-15 09:48:43 -0700

    added error handling. Not very DRY, but will improve later

--- budget-calculator.py
+++ budget-calculator.py
@@ -12,19 +12,47 @@ class Budget:
         left_over = self.paycheck - expenses
         return left_over
 
+# calculate expenses vs savings compares to ratio and give recommendation if they should save more or are good
+# visually show this to user in a pie chart
+# error handle
+
+
+def idiot_proof(num):
+    try:
+        correct_num = float(num)
+        return correct_num
+    except ValueError:
+        print("Please enter a number")
+        try:
+            num = float(input())
+            return num
+        except:
+            print("Program has been stopped due to invalid response. Please run again")
+            return
+
 
 def budget_calculator():
     print("Welcome to the budget calculator! We're going to ask you questions about your monthly spending and tell you how much you'll have left to save.")
     print("How much do you earn a month?")
-    paycheck = int(input())
+    paycheck = idiot_proof(input())
+    if(paycheck == None):
+        return
     print("How much do you spend on groceries a month?")
-    groceries = int(input())
+    groceries = idiot_proof(input())
+    if(groceries == None):
+        return
     print("How much do you spend on reoccuring bills?")
-    bills = int(input())
+    bills = idiot_proof(input())
+    if(bills == None):
+        return
     print("How much do you spend on yourself?")
-    personal = int(input())
+    personal = idiot_proof(input())
+    if(personal == None):
+        return
     print("Anything else you spend per month? If yes, enter the amount, if no enter 0")
-    extra = int(input())
+    extra = idiot_proof(input())
+    if(extra == None):
+        return
     user_budget = Budget(paycheck, groceries, bills, personal, extra)
     amt_left = str(user_budget.how_much_left())
     print("You will have $" + amt_left + " after all of your expenses")
