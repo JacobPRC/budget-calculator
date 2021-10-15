cac0b24df77ebeaa5da86b7a59378a63ea48cbc6
jacob <jacobcunningham96@yahoo.com>
2021-10-15 09:56:14 -0700

    cleaned error handling

--- budget-calculator.py
+++ budget-calculator.py
@@ -14,7 +14,7 @@ class Budget:
 
 # calculate expenses vs savings compares to ratio and give recommendation if they should save more or are good
 # visually show this to user in a pie chart
-# error handle
+# find DRY way to exit parent function if error. Raise exception?
 
 
 def idiot_proof(num):
@@ -27,32 +27,28 @@ def idiot_proof(num):
             num = float(input())
             return num
         except:
-            print("Program has been stopped due to invalid response. Please run again")
-            return
+            raise Exception(
+                "Program has been stopped due to invalid response. Please run again")
 
 
 def budget_calculator():
     print("Welcome to the budget calculator! We're going to ask you questions about your monthly spending and tell you how much you'll have left to save.")
+
     print("How much do you earn a month?")
     paycheck = idiot_proof(input())
-    if(paycheck == None):
-        return
+
     print("How much do you spend on groceries a month?")
     groceries = idiot_proof(input())
-    if(groceries == None):
-        return
+
     print("How much do you spend on reoccuring bills?")
     bills = idiot_proof(input())
-    if(bills == None):
-        return
+
     print("How much do you spend on yourself?")
     personal = idiot_proof(input())
-    if(personal == None):
-        return
+
     print("Anything else you spend per month? If yes, enter the amount, if no enter 0")
     extra = idiot_proof(input())
-    if(extra == None):
-        return
+
     user_budget = Budget(paycheck, groceries, bills, personal, extra)
     amt_left = str(user_budget.how_much_left())
     print("You will have $" + amt_left + " after all of your expenses")
